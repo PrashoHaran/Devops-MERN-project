846bb873ee48796b1178e42cb3018a0530245f93
PrashoHaran <prashoharan8962@gmail.com>
2026-02-16 01:42:25 +0530

    test files added

---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const {server, app} = require('../index');
+const mongoose = require('mongoose'); 
+
+describe('GET api/tasks', () => {
+    it('should return 200 ok', async () => {
+        const res = await request(app).get('/api/tasks')
+        expect(res.statusCode).toBe(200);
+        });
+
+    it('It should return object and tasks properties', async () => {
+        const res = await request(app).get('/api/tasks')
+        expect(typeof res.body).toBe("object");
+        expect(res.body).toHaveProperty('tasks');
+    });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close(); 
+    await server.close(); 
+});
